d15023928731d5f7078a35a063996506b350dbe4
aleenaritgithub <125199630+aleenaritgithub@users.noreply.github.com>
2024-02-20 16:09:19 +0500

    recaptcha

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function beforesubmit(){
     let formatdate = new Date(inputdate.value).toLocaleDateString("en-AU");
     outputdate.value = formatdate;
 }
+
+function timestamp() 
+{ var response = document.getElementById("g-recaptcha-response"); 
+if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+        document.getElementsByName("captcha_settings")[0].value
+        );
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = 
+        JSON.stringify(elems); 
+    } 
+} 
+setInterval(timestamp, 500); 
